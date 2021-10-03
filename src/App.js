137aaa228d2a5c77a48efff2454b6bf5b3c42c2a
josue <josue.ramirez.z@uni.pe>
2021-10-03 10:21:26 -0500

    uso del react context de la primera forma

--- src/App.js
+++ src/App.js
@@ -2,47 +2,44 @@ import { ToDoList } from './ToDoList';
 import { ToDoItem } from './ToDoItem';
 import './App.css';
 import { ToDoSearch } from './ToDoSearch';
+import { TodoContext } from './Context';
+import { TodoProvider } from './Context';
 import React from 'react';
-const todos =[
-  {text:' hola',completed: false },
-  {text:' gua',completed: false },
-  {text:'miau',completed: false },
-]
-function App() {
-  const [lista, setLista]=React.useState(todos);
-  const [searchvalue, setSearchValue]= React.useState('')
-  
-  let searchedTodo= [];
 
-  (!searchvalue.length>=1)
-  ?searchedTodo=lista
-  :searchedTodo = lista.filter(
-    todo=>{
-    const todoText =todo.text.toLowerCase();
-    const searchvalueLower= searchvalue.toLowerCase();
-     return todoText.includes(searchvalueLower)
-    })
-  const deleteTodo = (text) => {
-    const todoIndex = lista.findIndex(todo => todo.text === text);
-    const newTodos = [...lista];
-    newTodos.splice(todoIndex, 1);
-    setLista(newTodos);
-  }; 
- 
-   return ( 
-   <React.Fragment>
-    <ToDoList>
-        <ToDoSearch   setSearchValue={setSearchValue}   />
-        {searchedTodo.map((todo)=>   
-          <ToDoItem 
-          key={todo.text} 
-          text={todo.text}
-          completed={todo.completed} 
-          onDelete={()=>deleteTodo(todo.text)} 
-          />
-          )}  
-      </ToDoList>
-    </React.Fragment>
+function App() { 
+/*   const {
+    lista,
+    searchvalue,
+    setLista,  
+    searchedTodo,
+    deleteTodo,
+    setSearchValue,
+  } =React.useContext(TodoContext); */
+  
+  return ( 
+    <TodoProvider>
+      <React.Fragment>
+      <ToDoSearch    /* setSearchValue={setSearchValue }*//>
+          <TodoContext.Consumer>
+          {({lista,
+            searchvalue,
+            setLista,  
+            searchedTodo,
+            deleteTodo,
+            setSearchValue,})=>(<ToDoList>
+          {searchedTodo.map((todo)=>   
+            <ToDoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed} 
+            onDelete={()=>deleteTodo(todo.text)} 
+            />
+          )}
+          </ToDoList>
+          )}
+          </TodoContext.Consumer>
+      </React.Fragment>
+    </TodoProvider>
   );   
  } 
  
--- src/Context.js
+++ src/Context.js
@@ -1,14 +1,44 @@
 import React from "react";
 
-const TodoContext = React.useContext();
+const TodoContext = React.createContext();
 
 function TodoProvider(props){
+   const todos =[
+      {text:' hola',completed: false },
+      {text:' gua',completed: false },
+      {text:'miau',completed: false },
+    ]
+   const [lista, setLista]=React.useState(todos);
+   const [searchvalue, setSearchValue]= React.useState('')
+   
+   let searchedTodo= [];
+ 
+   (!searchvalue.length>=1)
+   ?searchedTodo=lista
+   :searchedTodo = lista.filter(
+     todo=>{
+     const todoText =todo.text.toLowerCase();
+     const searchvalueLower= searchvalue.toLowerCase();
+      return todoText.includes(searchvalueLower)
+     })
+   const deleteTodo = (text) => {
+     const todoIndex = lista.findIndex(todo => todo.text === text);
+     const newTodos = [...lista];
+     newTodos.splice(todoIndex, 1);
+     setLista(newTodos);
+   }; 
+   
    return(
-      <TodoContext.TodoProvider value={{
-         
+      <TodoContext.Provider value={{
+            lista,
+            searchvalue,
+            setLista,
+            searchedTodo,
+            deleteTodo,
+            setSearchValue,
       }}>
-         {props.childrem}
-      </TodoContext.TodoProvider>
+         {props.children}
+      </TodoContext.Provider>
    )
 }
-export {TodoProvider}
+export { TodoContext,TodoProvider}
